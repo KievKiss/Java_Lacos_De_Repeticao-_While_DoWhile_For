42114dbeff3af71a1679d1fa44de4ad7d8035cfa
Kiev <kevinkiss2010@gmail.com>
2023-11-10 15:51:35 -0300

    Adicionando Exercicios de While, for e Do While

--- Ex01LacoDeRepeticaoFor.java
+++ Ex01LacoDeRepeticaoFor.java
@@ -1,7 +1,7 @@
 /*Escreva um algoritmo em Java, que leia 2 números inteiros via teclado, onde o primeiro número 
  *deve ser menor do que o segundo número. Caso contrário, deve ser exibida uma mensagem na tela
  *informando que o intervalo é inválido e sair do programa.
- *No intervalo informado, mostre na tela todes os números que são múltiplos de 3 e 5*/
+ *No intervalo informado, mostre na tela todos os números que são múltiplos de 3 e 5*/
 
 
 package LacoDeRepeticaoExercicios;
